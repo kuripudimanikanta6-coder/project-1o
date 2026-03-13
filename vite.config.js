d4d6a2694395caaa35d8461847cbb0eefe4f98c9
2200031216 <2200031216@kluniversity.in>
2026-03-13 14:44:33 +0530

    fix pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/project-1o/',
   plugins: [react()],
-  base: '/project/',
 })
