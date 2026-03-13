32ff8c5a27643ced6d1916b543b55b4827015948
2200031216 <2200031216@kluniversity.in>
2026-03-13 15:27:51 +0530

    deploy site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/project-1o/',
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+}
+);
